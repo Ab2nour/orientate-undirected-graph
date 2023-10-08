984c50c67f7474bd27001cc954212a7203f72a7c
Ab2nour <61651582+Ab2nour@users.noreply.github.com>
2023-10-08 18:58:59 +0200

    refactor: improve code drawing graphs

--- src/functions.py
+++ src/functions.py
@@ -5,27 +5,11 @@ import networkx as nx
 import numpy as np
 
 # Our default presets
-COLORS: Final[dict[str, str]] = {
+color_options: Final[dict[str, str]] = {
     "node_color": "#ffffff",
-    "node_border_color": "#111827",
+    "edgecolors": "#111827",
     "edge_color": "#94a3b8",
 }
-
-
-def _draw_graph(
-    graph: nx.Graph, node_color: str, node_border_color: str, edge_color: str
-) -> None:
-    """Draws the graph `graph` with the color parameters specified.
-
-    Example
-    ---
-    >>> example = nx.Graph([(1, 2), (1, 3), (2, 3)])
-    >>> _draw_graph(example, "white", "black", "gray")
-    """
-    nx.draw(graph, node_color=node_color, edge_color=edge_color)
-
-    ax = plt.gca()
-    ax.collections[0].set_edgecolor(node_border_color)
 
 
 def draw_graph(graph: nx.Graph) -> None:
@@ -36,8 +20,11 @@ def draw_graph(graph: nx.Graph) -> None:
     >>> example = nx.Graph([(1, 2), (1, 3), (2, 3)])
     >>> draw_graph(example)
     """
-    _draw_graph(
-        graph, COLORS["node_color"], COLORS["node_border_color"], COLORS["edge_color"]
+    plt.axis("off")
+
+    nx.draw_networkx(
+        graph,
+        **color_options
     )
 
 
